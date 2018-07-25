2093fc76c19c6314f50ac835cca662c1768ddbe8
Toby Petty <tobypetty@hotmail.com>
2018-07-25 13:08:04 -0400

    fixed new-homes-for-sale url issue, updated examples, added tests

--- package/setup.py
+++ package/setup.py
@@ -9,38 +9,25 @@ The package is intended for use by data analysts/scientists who require easy acc
 """
 
 setup(
-    name='rightmove_webscraper',
-    packages=['rightmove_webscraper'],  # Must be same as name
-    version='0.1',
-    description='A class for scraping data from rightmove.co.uk',
+    name="rightmove_webscraper",
+    packages=["rightmove_webscraper"],
+    version="0.3",
+    description="A class for scraping data from rightmove.co.uk",
     long_description=long_description,
-    author='Toby Petty',
-    author_email='hello@tobypetty.com',
-    url='https://github.com/woblers/rightmove_webscraper.py',
-    # This specifies which other Pip packages your package
-    # requires to be installed:
+    author="Toby Petty",
+    author_email="hello@tobypetty.com",
+    url="https://github.com/woblers/rightmove_webscraper.py",
     install_requires=[
-        'pandas',
-        'requests',
-        'lxml'
+        "pandas",
+        "requests",
+        "lxml"
     ],
-    keywords=['webscraping', 'rightmove', 'data'],
-    license='MIT',
+    keywords=["webscraping", "rightmove", "data"],
+    license="MIT",
     classifiers=[
-        # For full list of relevant classifiers, see:
-        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-
-        # How mature is this project?
-        'Development Status :: 4 - Beta',
-
-        # Who is the project intended for?
-        'Intended Audience :: End Users/Desktop',
-
-        # Pick license (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
-        # Specify Python versions supported. In particular, ensure
-        # to indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
     ],
 )
